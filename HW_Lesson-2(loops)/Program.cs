4162742688408713faca0b51e2c82a8f828eae36
MeirLessons <85080596+MeirLessons@users.noreply.github.com>
2021-06-22 18:15:20 +0300

    Add files via upload

---
+++ HW_Lesson-2(loops)/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Lesson_2_loops_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Print Avg
+
+            //int sum = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    sum += int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Avg: {0}", sum/5);
+
+            #endregion
+
+            #region 0-9 , 0-8, 0-7...
+
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Multiplication Table
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+                Console.WriteLine();
+            }
+            //i = 2; j = 2;
+            //1   2   3   4    5...10  
+            //2   4   6   8    10..20
+            //3   6   9   12.......30
+            #endregion
+
+            #region School Pupils
+
+            int numOfPupils = int.Parse(Console.ReadLine());
+            int classesCounter = 0;
+
+            //while(numOfPupils / 10 > 0)
+            //{
+            //    classesCounter++;
+            //    if (numOfPupils >= 100)
+            //    {
+            //        numOfPupils -= 100;
+            //    }
+            //    else if (numOfPupils >= 30)
+            //    {
+            //        numOfPupils -= 30;
+            //    }
+            //    else
+            //        numOfPupils -= 10;
+            //}
+            for (int i = 0; i <= numOfPupils / 100; i++)
+            {
+                classesCounter++;
+                numOfPupils -= 100;
+            }
+            for (int i = 0; i <= numOfPupils / 30; i++)
+            {
+                classesCounter++;
+                numOfPupils -= 30;
+            }
+            for (int i = 0; i <= numOfPupils / 10; i++)
+            {
+                classesCounter++;
+                numOfPupils -= 10;
+            }
+            Console.WriteLine("Classes Needed: {0}", classesCounter);
+            Console.WriteLine("Without Class: {0}", numOfPupils);
+
+            #endregion
+
+            #region ETGAR: Print Vertical Triangle
+            int num = int.Parse(Console.ReadLine());
+
+            int spaces = num / 2;
+            int printSymbols = num - (spaces*2);
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int y = 0; y < printSymbols; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                spaces--;
+                printSymbols += 2;
+                if (spaces < 0)
+                    break;
+            }
+            #endregion
+        }
+    }
+}
---
+++ Lesson-3 Arrays/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());    
+            int.Parse(Console.ReadLine());
+            Console.WriteLine(@"Please Enter First\Last Name");
+            int[] arrrr = new int[2] { 3, 5 };
+            int myint = 24;
+            int myint2 = 30;
+            int myint3 = 40;
+            //Console.WriteLine("Value 1 Is: {0}. Value 2 Is: {1}. Value 3 Is: {2}",myint,myint2,myint3);
+            Console.WriteLine($"Value 1 Is: \"{arrrr[0]}.\"\n Value 2 Is: \"{myint2}\".\n Value 3 Is: \"{myint3}\"\t end");
+
+
+            string defaultStrValue = null;
+            int defaultNumberValue = 0;
+            bool defaultBoolValue = false;
+            char defaultCharValue = '0';
+
+            //***************************************************
+
+            //1:
+            //מגדיר גודל של מערך בלי ערכים לאיברים(כל האיברים יקבלו דיפולט)
+            bool[] myArr = new bool[5];
+            myArr[0] = true;
+            myArr[3] = true;
+            Console.WriteLine(myArr.Length);
+
+            //2:
+            //הגדרה של מערך עם נתינת ערכים ישר ביצירה (גודל המערך ייקבע לפי כמות הערכים שאתן)
+            string[] myStrArr = {"1","Moshe","","Hackeru"};
+                               //[0]  [1]    [2]   [3] 
+            myStrArr[0] = "new 1";
+
+            //3:
+            // כול את שיטה 1 ושיטה 2 ביחד - מונע בלבולים
+            int[] grades = new int[3] { 100,90,85 };
+
+            //4:
+            // הגדרת מערך ללא התחייבות לגודל וללא נתינת ערכים
+            char[] myChars;
+            int size = int.Parse(Console.ReadLine());//36
+            myChars = new char[size];// יוצר את המערך
+            myChars[0] = 'f';
+            
+        }
+    }
+}
